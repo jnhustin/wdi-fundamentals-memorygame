9b9bf69f00cc492e04177f0458a5bfa5f1dd3d36
Justin Hoang <JustinHoang@Minolath.local>
2017-01-24 11:22:20 -0800

    added JS card creation

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,15 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+	for (var i = 0; i < 4; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
+}
+
+createCards();
